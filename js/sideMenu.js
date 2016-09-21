2d7cfeb1f5a79abe1d8c933751ed408953202dca
Dariusz Franczak <[darius32@op.pl]>
2016-09-21 12:25:28 +0100

    additional dash boards added

---
+++ js/sideMenu.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+      var trigger = $('.hamburger'),
+      isClosed = false,
+      sidebarWidth = 70;
+
+    trigger.click(function () {
+      hamburger_cross();      
+    });
+
+    function hamburger_cross() {
+
+      if (isClosed == true) {          
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {   
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
+  
+  $('[data-toggle="offcanvas"]').click(function () {
+        
+        $('#wrapper').toggleClass('toggled');
+
+        if($('#wrapper').hasClass('toggled')){          
+          $('.side-nav-column').removeClass( "col-lg-1");
+          $('.whole-page-column').removeClass( "col-lg-11");
+          $('.side-nav-column').addClass( "col-lg-2");
+          $('.whole-page-column').addClass( "col-lg-10");
+          $('#sidebar-wrapper').width($('#wrapper').width());
+        } else {
+          $('.side-nav-column').removeClass( "col-lg-2" ).addClass( "col-lg-1");
+          $('.whole-page-column').removeClass( "col-lg-10" ).addClass( "col-lg-11");
+          $('#sidebar-wrapper').width(sidebarWidth);
+        }
+
+  });  
+
+
+  $('#sidebar-wrapper').hover(function () {
+
+        $('#wrapper').toggleClass('toggled');
+
+        if($('#wrapper').hasClass('toggled')){          
+          $('.side-nav-column').removeClass( "col-lg-1" ).addClass( "col-lg-2" );
+          $('.whole-page-column').removeClass( "col-lg-11" ).addClass( "col-lg-10" );
+          $('#sidebar-wrapper').width($('#wrapper').width());
+        } else {
+          $('#sidebar-wrapper').width(sidebarWidth);
+          $('.side-nav-column').removeClass( "col-lg-2" ).addClass( "col-lg-1" );
+          $('.whole-page-column').removeClass( "col-lg-10" ).addClass( "col-lg-11" );
+        }
+
+  }); 
+
+  $(".dropdown-toggle").dropdown();
+});
